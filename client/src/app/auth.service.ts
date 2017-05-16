7930be6900f91a41a9bd0cdf33d70819b12bd12b
Kaitaku Takeda <katakeda@ucsc.edu>
2017-05-15 19:07:36 -0700

    Changed http host to localhost:8080

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
     /*
     this.http.post(url, body, option) returns an Observable. Think of an Observable as a stream of events published by some source. To listen for events in this stream, subscribe to the Observable. These subscriptions specify the actions to take when the web request produces a success event (with the hero data in the event payload) or a fail event (with the error in the payload).
     */
-    return this.http.post('http://ec2-107-23-73-244.compute-1.amazonaws.com:8080/users/register', user, {headers: headers})
+    return this.http.post('http://localhost:8080/users/register', user, {headers: headers})
     .map(res => res.json());
   }
 
@@ -28,7 +28,7 @@ export class AuthService {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    return this.http.post('http://ec2-107-23-73-244.compute-1.amazonaws.com:8080/users/authenticate', user, {headers: headers})
+    return this.http.post('http://localhost:8080/users/authenticate', user, {headers: headers})
     .map(res => res.json());
   }
 
@@ -37,7 +37,7 @@ export class AuthService {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    return this.http.get('http://ec2-107-23-73-244.compute-1.amazonaws.com:8080/users/profile', {headers: headers})
+    return this.http.get('http://localhost:8080/users/profile', {headers: headers})
     .map(res => res.json());
   }
 
