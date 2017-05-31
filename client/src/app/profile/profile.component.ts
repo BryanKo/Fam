6167535e866cbdc58b693f9250c822cd58e5b9b1
Anthony Lee <ant.lee813@gmail.com>
2017-05-31 02:27:25 -0700

    WIP
    Patch 2 (May 24, 2017)
      ++ added test profile data [ hardcoded ] in profile.component.html
    (Fam > client > src > app > profile)
      ++ sidebar.component.html code is commented and formatted
      ++ profile.component.ts adding component, import and changing oninit
    export class
        to attempt to get profile.html to feature feed
      ++ multitude of test code including hardcoded, formatting, and
    template from sidebar.component.html
    
      -- nothing removed
    
      ** commented on login.component.ts in regards to needs for change;
    UPDATE: FIXED
      /* PROBLEM: This is only hard-coded to jump straight to login page
    (map) after checking for correct user
                  input, but there is no pulling of actual user profile/
    data on the map/profile
      */

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core'; // added EventEmitter
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { RecoService } from '../reco.service'; // from sidebar.component.ts
 
 @Component({
   selector: 'app-profile',
@@ -11,11 +12,23 @@ export class ProfileComponent implements OnInit {
 
   user: any;
 
+  // from sidebar.component.ts
+  name: string;
+  loc: string;
+  // lat: number;
+  // lng: number;
+  star: number;
+  categ: string;
+  desc: string;
+  direct: string;
+
   constructor(
+    private recoService: RecoService, // added from sidebar.component.ts
     private router: Router,
     private authService: AuthService
   ) { }
 
+  // untouched
   ngOnInit() {
     if(!localStorage.getItem('user')) {
       this.router.navigate(['/']);
