8e1356489f4226ef4b455ee0963cbaea0aa789d7
Bryan Ko <bkko@ucsc.edu>
2017-05-31 12:38:15 -0700

    pass login info to reco database and edited format of reviews

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { RecoService } from '../reco.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +11,12 @@ import { AuthService } from '../auth.service';
 export class ProfileComponent implements OnInit {
 
   user: any;
+  username: string;
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private recoService: RecoService
   ) { }
 
   ngOnInit() {
