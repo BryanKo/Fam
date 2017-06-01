3f631e01ad949cfa9fdfc7d20b4daa1d95339c4f
Bryan Ko <bkko@ucsc.edu>
2017-06-01 13:35:51 -0700

    fixing merging conflight between anthony and bryan

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { RecoService } from '../reco.service';
@@ -8,39 +8,29 @@ import { RecoService } from '../reco.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
 
   user: any;
-  username: string;
-
-  // from sidebar.component.ts
-  name: string;
-  loc: string;
-  // lat: number;
-  // lng: number;
-  star: number;
-  categ: string;
-  desc: string;
-  direct: string;
-
+  recos: any;
+  
   constructor(
-    private recoService: RecoService, // added from sidebar.component.ts
     private router: Router,
     private authService: AuthService,
+    private recoService: RecoService
   ) { }
 
-  // Gets the list of all recos from MainComponent
-  //@Input()
-  //recosList: any;
-
-
-  // untouched
   ngOnInit() {
     if(!localStorage.getItem('user')) {
       this.router.navigate(['/']);
     }
 
     this.user = JSON.parse(localStorage.getItem('user'));
+
+    this.recoService.getRecos().subscribe(recos => {
+      this.recos = recos;
+    });
+
   }
 
 }
