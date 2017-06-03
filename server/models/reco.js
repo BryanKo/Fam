80a13ac67d8696c0cec2353f33f92d889f6c41cb
Bryan Ko <bkko@ucsc.edu>
2017-06-02 19:46:39 -0700

    minor changes

--- server/models/reco.js
+++ server/models/reco.js
@@ -16,14 +16,15 @@ const RecoSchema = mongoose.Schema({
   },
   lat:{
     type: Number,
-    // required: true
+    required: true
   },
   lng:{
     type: Number,
-    // required: true
+    required: true
   },
   stars:{
     type: Number,
+    required: true
   },
   categ:{
     type: String,
@@ -35,6 +36,7 @@ const RecoSchema = mongoose.Schema({
   },
   direct:{
     type:String,
+    required: true
   },
   review:{
     type: String,
